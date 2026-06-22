a07c218ff45f58abaffd57e5de1c7624fb31782c
Rick M <kingthorin@users.noreply.github.com>
2026-06-22 10:03:42 -0400

    Fix some links

--- .github/workflows/scripts/check_links.py
+++ .github/workflows/scripts/check_links.py
@@ -66,6 +66,9 @@ def validate_url(url, context):
     try:
         print(f"Checking URL: {url} (Context: {context})")
         response = requests.head(url, allow_redirects=True, timeout=10)
+        if (response.status_code == 405) {
+            response = requests.get(url, allow_redirects=True, timeout=10)
+        }
 
         # Check if URL was redirected
         if response.history:
